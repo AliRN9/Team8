a995efc1a318e60351340416e164c1f6d1d84ff7
Maxim <grigorievma9461@gmail.com>
2024-04-04 17:53:52 +0300

    add plug

--- pkg/handler/modelData.go
+++ pkg/handler/modelData.go
@@ -1,12 +1,13 @@
 package handler
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"math/rand"
 	Team8 "middlewareGolang"
 	"net/http"
+	"time"
 )
 
 func (h *Handler) getPredict(c *gin.Context) {
@@ -41,23 +42,50 @@ const URL = "*"
 const ContentType = "application/json"
 
 func sendModelToApp(model Team8.Model) ([]byte, error) {
-	jsonData, err := json.Marshal(model)
-	if err != nil {
-		return nil, fmt.Errorf("error marshaling JSON")
+	type Response struct {
+		Status string `json:"Status"`
 	}
 
-	resp, err := http.Post(URL, ContentType, bytes.NewBuffer(jsonData))
-	if err != nil {
-		return nil, fmt.Errorf("error sending model to app")
+	source := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(source)
+	number := random.Intn(11)
+
+	var response Response
+	if number > 5 {
+		response = Response{
+			Status: "1",
+		}
+	} else {
+		response = Response{
+			Status: "0",
+		}
 	}
 
-	defer resp.Body.Close()
-
-	var responseBody []byte
-	decoder := json.NewDecoder(resp.Body)
-	if err := decoder.Decode(&responseBody); err != nil {
-		return nil, fmt.Errorf("error decoding response")
+	jsonData, err := json.Marshal(response)
+	if err != nil {
+		fmt.Println("Ошибка при маршалинге JSON:", err)
+		return nil, nil
 	}
 
 	return jsonData, nil
+
+	//jsonData, err := json.Marshal(model)
+	//if err != nil {
+	//	return nil, fmt.Errorf("error marshaling JSON")
+	//}
+	//
+	//resp, err := http.Post(URL, ContentType, bytes.NewBuffer(jsonData))
+	//if err != nil {
+	//	return nil, fmt.Errorf("error sending model to app")
+	//}
+	//
+	//defer resp.Body.Close()
+	//
+	//var responseBody []byte
+	//decoder := json.NewDecoder(resp.Body)
+	//if err := decoder.Decode(&responseBody); err != nil {
+	//	return nil, fmt.Errorf("error decoding response")
+	//}
+	//
+	//return jsonData, nil
 }
