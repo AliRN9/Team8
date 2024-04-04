3361790e73e261953475e64d093d07798a1383bb
Maxim <grigorievma9461@gmail.com>
2024-04-04 19:12:14 +0300

    add cors

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"github.com/gin-gonic/gin"
 	"middlewareGolang/pkg/service"
+	"net/http"
 )
 
 type Handler struct {
@@ -18,8 +19,24 @@ func (h *Handler) InitRoutes() *gin.Engine {
 
 	api := router.Group("/api")
 	{
-		api.POST("/predict", h.getPredict)
+		api.POST("/predict", CORS(h.getPredict))
 	}
 
 	return router
+}
+
+func CORS(next gin.HandlerFunc) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		next(c)
+	}
 }
